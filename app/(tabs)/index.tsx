aaddd2505d9bbfbfb8c5e77a21c5525a6eb14d94
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-05-19 15:03:29 +0300

    completed home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '@/components/Header'
-import { Image, ScrollView, View, Dimensions, Text, TouchableOpacity } from 'react-native'
-import { BANNERS } from './../../assets/assets';
+import { Image, ScrollView, View, Dimensions, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { BANNERS, dummyProducts } from './../../assets/assets';
 import { useRouter } from 'expo-router';
 import { CATEGORIES } from '@/constants';
 import CategoryItem from '@/components/CategoryItem';
+import ProductCard from '@/components/ProductCard';
 
 
 const { width: ScreenWidth } = Dimensions.get('window');
@@ -14,7 +15,26 @@ export default function Home() {
   const router = useRouter();
   const categories = [{ id: "all", name: 'All', icon: 'grid' }, ...CATEGORIES];
 
+  const [products, setProduct] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
   const [activeBannerIndex, setActiveBannerIndex] = React.useState(0);
+
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
+      setProduct(dummyProducts)
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
     <SafeAreaView style={{ flex: 1 }} edges={['top']}>
       <Header title="Home" showCart showMenu showLogo />
@@ -62,27 +82,58 @@ export default function Home() {
               ))
             }
           </View>
-
-          {/* Categories section will be here. */}
-          <View className='mb-4'>
-            <View className='flex-row justify-between mb-4 item-center '>
-              <Text className='text-xl font-bold text-primary'>Categories</Text>
-            </View>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false} className='w-full h-32'>
-              {categories.map((category) => (
-                <TouchableOpacity key={category.id} className='mr-4'>
-                  <CategoryItem item={category}
-                    isSelected={category.id === 'all'}
-                    onPress={() => router.push({
-                      pathname: `/shop/`,
-                      params: { category: category.id === "all" ? "" : category.id }
-                    })} />
-                </TouchableOpacity>
-              ))}
-            </ScrollView>
-          </View>
         </View>
 
+        {/* Categories section will be here. */}
+        <View className='mb-4'>
+          <View className='flex-row justify-between mb-4 item-center '>
+            <Text className='text-xl font-bold text-primary'>Categories</Text>
+          </View>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} className='w-full h-32'>
+            {categories.map((category) => (
+              <TouchableOpacity key={category.id} className='mr-4'>
+                <CategoryItem item={category}
+                  isSelected={category.id === 'all'}
+                  onPress={() => router.push({
+                    pathname: `/shop/`,
+                    params: { category: category.id === "all" ? "" : category.id }
+                  })} />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+
+        {/* Popular Products section will be here. */}
+        <View className='mb-8'>
+          <View className='flex-row items-center justify-between mb-4'>
+            <Text className='text-xl font-bold text-primary'>Popular</Text>
+            <TouchableOpacity onPress={() => router.push("/shop")}>
+              <Text className='text-sm text-secondary'>See All</Text>
+            </TouchableOpacity>
+          </View>
+
+          {loading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : (
+            <View className='flex-row flex-wrap justify-between'>
+              {products.slice(0, 4).map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </View>
+          )}
+
+        </View>
+
+        {/* Newsletter  CTA. */}
+        <View className="items-center p-6 mb-20 bg-gray-100 rounded-2xl">
+          <Text className='mb-2 text-2xl font-bold text-center text-primary'>Join Our Newsletter</Text>
+          <Text className='mb-4 text-sm text-center text-secondary'>
+            Subscribe to our newsletter for the latest updates and exclusive offers!
+          </Text>
+          <TouchableOpacity className='items-center w-4/5 py-3 rounded-full bg-primary'>
+            <Text className='text-white font-base medium text-'>Subscribe</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
---
+++ components/ProductCard.tsx
@@ -0,0 +1,44 @@
+import { View, TouchableOpacity, Image, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { COLORS } from '@/constants';
+
+export default function ProductCard({ product }: { product: any }) {
+    const [isLiked, setIsLiked] = React.useState(false);
+
+    return (
+        <Link href={`/product/${product._id}`} asChild>
+            <TouchableOpacity className='w-[48%] mb-4 bg-white rounded-lg overflow-hidden '>
+                <View className='relative w-full h-56 bg-gray-100'>
+                    <Image source={{ uri: product.images[0] }}
+                        className='w-full h-full' resizeMode="cover" />
+
+                    {/* Favorite icon   */}
+                    <TouchableOpacity className='absolute z-10 p-2 bg-white rounded-full shadow-sm top-2 right-2' onPress={(e) => e.stopPropagation()}>
+                        <Ionicons name={`${isLiked ? 'heart' : 'heart-outline'}`} size={24} color={`${isLiked ? COLORS.accent : COLORS.primary}`} />
+                    </TouchableOpacity>
+
+                    {/* is Featured */}
+                    {product.isFeatured && (
+                        <View className='absolute top-0 left-0 z-10 px-2 py-1 rounded-bl-lg bg-accent'>
+                            <Text className='text-xs font-bold text-white'>Featured</Text>
+                        </View>
+                    )}
+                </View>
+                {/* Product details */}
+                <View className='p-3'>
+                    <View className='flex-row items-center mb-1'>
+                        <Ionicons name="star" size={14} color="#FFD700" />
+                        <Text className='ml-1 text-sm text-gray-600'>{product.ratings.average.toFixed(1)} ({product.ratings.count})</Text>
+                    </View>
+                    <Text className='mb-1 text-sm font-medium text-primary' numberOfLines={1}>{product.name}</Text>
+                    <View >
+                        <Text className='text-base font-bold text-pretty'>${product.price.toFixed(2)}</Text>
+                    </View>
+
+                </View>
+            </TouchableOpacity>
+        </Link>
+    )
+}
