2a4b40c9d34c84dd1044dca3292b373dac28492a
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-05-28 20:03:50 +0300

    fix(server): improve formatting and add webhook route for Clerk integration

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,30 @@
 import "dotenv/config";
-import express,{ type Request, type Response }from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhook } from "./controllers/webhook.js";
 
 const app = express();
 
 //connect to database
 await connectDB();
 
-
+//webook route
+app.post("/api/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 // Clerk middleware
 app.use(clerkMiddleware());
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
