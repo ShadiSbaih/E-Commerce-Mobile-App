988d2476d2d454c6ece9340f41040a1ab784a3f7
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-05-28 14:10:34 +0300

    feat(webhook): enhance Clerk webhook handling and user model integration feat(types): add user and other interfaces for better type safety feat(vercel): configure Vercel deployment settings

--- server/controllers/webhook.ts
+++ server/controllers/webhook.ts
@@ -1,22 +1,39 @@
-import { verifyWebhook } from "@clerk/express/webhooks"
-import type { Request, Response } from "express"
-
-
+import { verifyWebhook } from "@clerk/express/webhooks";
+import type { Request, Response } from "express";
+import User from "../models/User.js";
 
 export const clerkWebhook = async (req: Request, res: Response) => {
   try {
-    const event = await verifyWebhook(req)
+    const event = await verifyWebhook(req);
+    if (event.type === "user.created" || event.type === "user.updated") {
+      const user = await User.findOne({ clerkId: event.data.id });
+
+      const userData = {
+        clerkId: event.data.id,
+        name: event.data?.first_name + " " + event.data?.last_name,
+        email: event.data?.email_addresses?.[0]?.email_address,
+        image: event.data?.image_url,
+      };
+
+      if (user) {
+        await User.findByIdAndUpdate({clerkId: event.data.id}, userData);
+      } else {
+        await User.create(userData);
+      }
+    }
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = event.data
-    const eventType = event.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', event.data)
+    const { id } = event.data;
+    const eventType = event.type;
+    console.log(
+      `Received webhook with ID ${id} and event type of ${eventType}`,
+    );
+    console.log("Webhook payload:", event.data);
 
-    return res.send('Webhook received')
+    return res.send("Webhook received");
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return res.status(400).send('Error verifying webhook')
+    console.error("Error verifying webhook:", err);
+    return res.status(400).send("Error verifying webhook");
   }
-}
+};
---
+++ server/models/User.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import type { IUser } from "../types/index.js";
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image: { type: String },
+    clerkId: { type: String, required: true, unique: true, sparse: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true },
+);
+
+
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
