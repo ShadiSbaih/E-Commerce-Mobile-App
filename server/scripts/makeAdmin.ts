2a7fc6ad6fedc47ab7267ad40b9a0e604685ba61
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-05-28 20:38:10 +0300

    feat(server): add makeAdmin script to assign admin role to users

---
+++ server/scripts/makeAdmin.ts
@@ -0,0 +1,26 @@
+import { clerkClient } from "@clerk/express";
+import User from "../models/User.js";
+
+const makeAdmin = async () => {
+  try {
+    const email = process.env.ADMIN_EMAIL;
+    if (!email) {
+      console.error("ADMIN_EMAIL is not defined in environment variables.");
+      return;
+    }
+    const user = await User.findOneAndUpdate({ email }, { role: "admin" });
+   
+    if (user) {
+      await clerkClient.users.updateUserMetadata(user.clerkId as string, {
+        publicMetadata: { role: "admin" },
+      });
+      console.log(`User with email ${email} has been made an admin.`);
+    }
+    
+  } catch (err: any) {
+    console.error("Error making user an admin:", err?.message || err);
+  }
+};
+
+
+export default makeAdmin;
