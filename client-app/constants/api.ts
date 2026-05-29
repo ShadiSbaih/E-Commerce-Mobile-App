e581a9a9c5496d1371f84502cf4b788b111fee27
Shadi Sbaih <shadisbaih20014@gmail.com>
2026-05-29 13:47:16 +0300

    fix(api): Update the base URL to the deployment URL.

--- client-app/constants/api.ts
+++ client-app/constants/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { Platform } from "react-native";
+// import { Platform } from "react-native";
 
-const LOCAL_API_URL = Platform.select({
-  android: "http://192.168.1.2:3000/api",
-  ios: "http://192.168.1.2:3000/api",
-  default: "http://localhost:3000/api",
-});
+// const LOCAL_API_URL = Platform.select({
+//   android: "http://192.168.1.2:3000/api",
+//   ios: "http://192.168.1.2:3000/api",
+//   default: "http://localhost:3000/api",
+// });
 
-const api = axios.create({ baseURL: LOCAL_API_URL });
+const api = axios.create({ baseURL: "https://e-commerce-rho-three-16.vercel.app/api" });
 
 export default api;
